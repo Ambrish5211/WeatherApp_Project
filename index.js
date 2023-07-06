51f52d216b80d0923e16c0542ecfa0059ebbfff7
Ambrish Kumar <ambrish19.5211@gmail.com>
2023-07-06 22:11:22 +0530

    some features added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
-const apiKey = '83b12bf3c8ad6eda53f4aa5dad532948'
+const apiKey = '83b12bf3c8ad6eda53f4aa5dad532948';
 
 const city = document.querySelector('.searchBox');
-const weatherIcon = document.querySelector('.img-clouds')
+
 
  async function showWeather  ()  {
-  
+   
      const city = document.querySelector('.searchBox'); 
 
      let data = await fetch(apiUrl + `&appid=${apiKey}`+ `&q=${city.value}`);
@@ -15,25 +15,32 @@ const weatherIcon = document.querySelector('.img-clouds')
      document.querySelector('.city').innerHTML = data.name;
      document.querySelector('.js-humidity').innerHTML = data.main.humidity + `%`;
      document.querySelector('.js-wind').innerHTML = data.wind.speed + `km/hr`; 
-      
-     if(data.weather[0].main == "Clouds"){
-      weatherIcon.src = "./images/clouds.png";
-     }
-     else if(data.weather[0].main == "Clear"){
-      weatherIcon.src = "./images/clear.png";
-     }
-     else if(data.weather[0].main == "Rain"){
-      weatherIcon.src = "./images/rain.png";
-     }
-     else if(data.weather[0].main == "Drizzle"){
-      weatherIcon.src = "./images/drizzle.png";
-     }
-     else if(data.weather[0].main == "Mist"){
-      weatherIcon.src = "./images/mist.png";
-     }
-     else if(data.weather[0].main == "Snow"){
-      weatherIcon.src = "./images/snow.png";
-     }
+     var weatherIcon = document.querySelector('.img-clouds');
+     weatherIcon.src = "./images/rain.png"
+
+     switch (data.weather[0].main) {
+       
+      case "Clouds":
+         weatherIcon.src = "./images/clouds.png";
+          break;
+      case "Clear":
+          weatherIcon.src = "images/clear.png";
+          break;
+      case "Thunderstorm":
+          weatherIcon.src = "images/rain.png";
+          break;
+      case "Haze":
+          weatherIcon.src="images/drizzle.png";
+          break;
+      case "Mist":
+          weatherIcon.src="images/mist.png";
+          break;
+      default:
+          console.log("No weather icon available.");
+  }
+ const weather = document.querySelector('.weather');
+ weather.classList.remove('weather');
+ weather.classList.add('weather2');
 }
 
 
